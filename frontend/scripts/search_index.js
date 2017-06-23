e5faaddc86a8bb716b5b6896f1421f8a8a6176e2
fernandofavoretti <fernandofvprado@gmail.com>
2017-06-23 14:19:59 -0300

    fix: changing pages location

--- frontend/scripts/search_index.js
+++ frontend/scripts/search_index.js
@@ -2,7 +2,7 @@ function solr_response(data){
     first_movie = data.response.docs[0];
     titulo_comment = first_movie.titulo;
     id_movie = first_movie.id_movie;
-    window.location = '../../../../../crawler/frontend/components/details-movie.html?movie=' + id_movie ;
+    window.location = 'details-movie.html?movie=' + id_movie ;
 
 
 }
