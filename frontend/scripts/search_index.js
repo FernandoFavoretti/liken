f4d616497222d6ed4d09bec147c81da080881e6a
fernandofavoretti <fernandofvprado@gmail.com>
2017-06-23 15:37:26 -0300

    fix: changing solr call api

--- frontend/scripts/search_index.js
+++ frontend/scripts/search_index.js
@@ -1,9 +1,8 @@
-function solr_response(data){
+function solr_response_find(data){
     first_movie = data.response.docs[0];
     titulo_comment = first_movie.titulo;
     id_movie = first_movie.id_movie;
     window.location = 'details-movie.html?movie=' + id_movie ;
-
 
 }
 
@@ -11,10 +10,11 @@ $(document).ready(function(){
     $('#find_movie').on('click', find);
     function find(event) {
         movie_name = $('#search_movie_name').val();
-        solr_query ="http://104.198.178.60:8983/solr/linken/select?indent=on&q=titulo:"+movie_name+"&wt=json&callback=?&json.wrf=solr_response";
+        console.log(movie_name)
+        solr_query ="http://104.198.178.60:8983/solr/linken/select?indent=on&q=titulo:"+movie_name+"&wt=json&callback=?&json.wrf=solr_response_find";
         response = $.getJSON(solr_query);
-        console.log(response)
-        console.log(response)
+
+
     }
 });
 
--- frontend/scripts/submit_comment.js
+++ frontend/scripts/submit_comment.js
@@ -11,29 +11,31 @@ function findGetParameter(parameterName) {
     return result;
 }
 
-function solr_response(data){
+function solr_response_comment(data){
+
     first_movie = data.response.docs[0];
     titulo_comment = first_movie.titulo;
     id_movie_commented = first_movie.id_movie;
     actual_id = findGetParameter("movie");
     comment = $('#new_comment').val();
     var string_comment =id_movie_commented+"|"+comment+"|"+actual_id+"|"+titulo_comment;
-    url = "http://127.0.0.1:5000/linken/api/movies/addcomment/"+string_comment
-    $.getJSON(url);
-    $('.modal-ghost').fadeOut('100');
-    $('.modal').fadeOut('100');
-    location.reload();
+    url = "http://104.198.178.60:4001/linken/api/movies/addcomment/"+string_comment
+    teste = $.getJSON(url)
+    setTimeout(function(){
+        $('.modal-ghost').fadeOut('100');
+        $('.modal').fadeOut('100');
+        location.reload();
+    }, 1000);
+
 
 }
 
-$(document).ready(function(){
-    $('#add_comment').on('click', addcomment);
-    function addcomment(event) {
+function addcomment() {
         id_movie = findGetParameter("movie")
         movie_name = $('#comment_nome_filme').val(),
-        solr_query ="http://104.198.178.60:8983/solr/linken/select?indent=on&q=titulo:"+movie_name+"&wt=json&callback=?&json.wrf=solr_response";
-        response = $.getJSON(solr_query);
+        solr_query ="http://104.198.178.60:8983/solr/linken/select?indent=on&q=titulo:"+movie_name+"&wt=json&callback=?&json.wrf=solr_response_comment";
+        $.getJSON(solr_query);
 
-    }
-});
+}
+
 
